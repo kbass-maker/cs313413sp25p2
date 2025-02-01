aab6e9a704aa9858f32b6e07d9fa5fcda926ff4c
kbass4 <kbass4@luc.edu>
2025-01-31 18:09:16 -0600

    Calculating the average mean of the values.

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -97,9 +97,12 @@ public class TestIterator {
     list.add(66);
     double sum = 0;
     int n = 0;
-    // TODO use an iterator and a while loop to compute the average (mean) of the values
-    // (defined as the sum of the items divided by the number of items)
-    // testNonempty shows how to use an iterator; use i.hasNext() in the while loop condition
+    // Using an iterator and a while loop to compute the average (mean) of the values
+    final var i = list.iterator();
+    while (i.hasNext()) {
+      sum += i.next();
+      n++;
+    }
     assertEquals(61.3, sum / n, 0.1);
     assertEquals(7, n);
   }
